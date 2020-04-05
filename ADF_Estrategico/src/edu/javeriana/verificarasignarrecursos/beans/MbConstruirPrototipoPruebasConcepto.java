9386c05f8aace29926961f38b2829021648ffb1d
Wilman <willman.ortiz@outlook.com>
2020-04-04 19:22:43 -0500

    Mapea los objetos a la vista de ConstruirPrototipoPruebasConcepto

--- ADF_Estrategico/src/edu/javeriana/verificarasignarrecursos/beans/MbConstruirPrototipoPruebasConcepto.java
+++ ADF_Estrategico/src/edu/javeriana/verificarasignarrecursos/beans/MbConstruirPrototipoPruebasConcepto.java
@@ -1,7 +1,30 @@
 package edu.javeriana.verificarasignarrecursos.beans;
 
+import co.edu.javeriana.negocio.Prototipo;
+
+import javax.faces.event.ActionEvent;
+
 public class MbConstruirPrototipoPruebasConcepto {
+    
+    private Prototipo prototipo;
+    
     public MbConstruirPrototipoPruebasConcepto() {
         super();
+    }
+
+    public void onClickAgregarRecomendaciones(ActionEvent actionEvent) {
+        // Add event code here...
+    }
+
+    public void onClickEliminarRecomendaciones(ActionEvent actionEvent) {
+        // Add event code here...
+    }
+    
+    public void setPrototipo(Prototipo prototipo) {
+        this.prototipo = prototipo;
+    }
+
+    public Prototipo getPrototipo() {
+        return prototipo;
     }
 }
