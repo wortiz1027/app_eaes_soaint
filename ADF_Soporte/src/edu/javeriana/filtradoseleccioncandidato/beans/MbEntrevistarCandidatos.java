5c3f5c3ed386cc70e665227b994562ecd9a0e25b
Jhon Celemin <jhonceleminf@gmail.com>
2020-04-04 22:03:41 -0500

    Se mapean las vistas de soporte

--- ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEntrevistarCandidatos.java
+++ ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEntrevistarCandidatos.java
@@ -1,7 +1,20 @@
 package edu.javeriana.filtradoseleccioncandidato.beans;
 
-public class MbEntrevistarCandidatos {
+import co.edu.javeriana.negocio.Entrevista;
+
+
+public class MbEntrevistarCandidatos{
+    private Entrevista entrevista;
+    
     public MbEntrevistarCandidatos() {
         super();
+    }
+
+    public void setEntrevista(Entrevista entrevista) {
+        this.entrevista = entrevista;
+    }
+
+    public Entrevista getEntrevista() {
+        return entrevista;
     }
 }
--- ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEvaluarCurriculum.java
+++ ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEvaluarCurriculum.java
@@ -1,12 +1,24 @@
 package edu.javeriana.filtradoseleccioncandidato.beans;
 
+import co.edu.javeriana.negocio.Entrevista;
+
 import java.io.Serializable;
 
 public class MbEvaluarCurriculum implements Serializable {
     @SuppressWarnings("compatibility:9065134202316032382")
     private static final long serialVersionUID = 1L;
+    
+    private Entrevista entrevista;
 
     public MbEvaluarCurriculum() {
         super();
+    }
+
+    public void setEntrevista(Entrevista entrevista) {
+        this.entrevista = entrevista;
+    }
+
+    public Entrevista getEntrevista() {
+        return entrevista;
     }
 }
