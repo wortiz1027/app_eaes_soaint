7453d5c6ee90a5b3efdd5dd4031c4ef3dda50933
Brian <bcamilo@gmail.com>
2020-04-08 04:27:57 -0500

    Pantalla entrevistarCandidatos

--- ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEntrevistarCandidatos.java
+++ ADF_Soporte/src/edu/javeriana/filtradoseleccioncandidato/beans/MbEntrevistarCandidatos.java
@@ -1,8 +1,17 @@
 package edu.javeriana.filtradoseleccioncandidato.beans;
 
 import co.edu.javeriana.configuracion.extend.AbstractBPMManageBeanBase;
+import co.edu.javeriana.configuracion.utils.JsfUtils;
+import co.edu.javeriana.configuracion.utils.error.*;
+import co.edu.javeriana.facade.FacadeDatabase2;
+import java.math.BigDecimal;
+
+import co.edu.javeriana.negocio.Candidato;
 
 import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.event.ActionEvent;
 
@@ -14,6 +23,8 @@ public class MbEntrevistarCandidatos extends AbstractBPMManageBeanBase implement
     private static final long serialVersionUID = 1L;
     private static ADFLogger logger = ADFLogger.createADFLogger(MbEntrevistarCandidatos.class);
     
+    public List<Candidato> lstCandidatos;
+    
     public MbEntrevistarCandidatos() {
         super();
     }
@@ -21,7 +32,38 @@ public class MbEntrevistarCandidatos extends AbstractBPMManageBeanBase implement
     @Override
     public String inicializarManageBean() {      
         //TODO Realizar todas las inicializaciones y operaciones de arranque        
+        lstCandidatos = new ArrayList<>();
         return MbEntrevistarCandidatos.NAVEGACION_CONTINUAR;
+    }
+    
+    public void consultarCandidatosVacante(){
+        
+        try {
+            final String codigoVacante = (String) JsfUtils.getElObject("#{bindings.codigoVacante.inputValue}");
+            
+            FacadeDatabase2.consultarCandidatos(this.lstCandidatos, codigoVacante);
+            
+        } catch(Exception e) {
+            final ProcessError process = new ProcessError();
+            process.setMethodCode(ProcessError.MethodIdentifier.M03.getID());
+            process.process(e, this);
+        }
+    }
+    
+    public void insertarObservacionEntrevista() {
+        try {
+            
+            String response = "";
+            final String observaciones = (String) JsfUtils.getElObject("#{bindings.observaciones.inputValue}");
+            final String codigoEntrevista = (String) JsfUtils.getElObject("#{bindings.codigoEntrevista.inputValue}");
+            
+            FacadeDatabase2.actualizarEntrevista(new BigDecimal(codigoEntrevista), observaciones, response);
+            
+        } catch(Exception e) {
+            final ProcessError process = new ProcessError();
+            process.setMethodCode(ProcessError.MethodIdentifier.M03.getID());
+            process.process(e, this);
+        }
     }
     
     @Override
